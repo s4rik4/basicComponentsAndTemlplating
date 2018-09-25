ae42ecd784d7aa8d88c3f36b262758c5ff1a9b93
s4rik4 <csabi.sari@gmail.com>
2018-09-26 00:05:26 +0200

    2 way binding hozzáadás, id növelése

--- src/app/listdemo/listdemo.component.ts
+++ src/app/listdemo/listdemo.component.ts
@@ -1,36 +1,45 @@
 import { Component } from '@angular/core';
-export class Eventmodel {
-  id: number;
-  name: string;
-}
+import {EventModel} from './event-model';
+
+
 @Component({
   selector: 'app-listdemo',
   templateUrl: './listdemo.component.html',
   styleUrls: ['./listdemo.component.css']
 })
 export class ListdemoComponent {
-  events: Eventmodel[];
+  events: EventModel[];
   constructor() {
     this.events = [
       {
         id: 1,
-        name: 'sziget'
+        name: 'sziget',
+        pic: 'http://www.erdon.ro/2018/04/index111-1.jpg'
       },
       {
         id: 2,
-        name: 'fezen'
+        name: 'fezen',
+        pic: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSZpaTz2c4IhnrOxmAC3OAjP-nwDp8Hyi25WG-pHOYVJa5ZIljUaQ'
       },
       {
         id: 3,
-        name: 'rockmaraton'
+        name: 'rockmaraton',
+        pic: 'https://tixa.hu/kepek/0004/4101-1.jpg'
       }
     ];
     console.log('megjott a listdemo cmp');
+    const puf = this.events.reduce((x: EventModel, y: EventModel) => {
+      // (logikai vuzsgálat) ? haigaz :hanem
+      return x.id > y.id ? x : y;
+    }).id;
+    console.log(puf);
   }
-  add (newEventNameInput){
-    console.log(newEventNameInput);
+  add (newEventNameInput: HTMLInputElement){
+    const maxId = this.events.reduce((x, y) => x.id > y.id ? x : y).id;
+    this.events = [...this.events, new EventModel(maxId + 1, newEventNameInput.value)];
+    newEventNameInput.value = '';
   }
   delete(id: number) {
-    this.events = this.events.filter((ev: Eventmodel) => ev.id !== id);
+    this.events = this.events.filter((ev: EventModel) => ev.id !== id);
   }
 }

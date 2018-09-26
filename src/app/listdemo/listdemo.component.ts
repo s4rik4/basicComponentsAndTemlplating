c3aa4420a36b88d72d6b02d6ede6089eebf892bd
s4rik4 <csabi.sari@gmail.com>
2018-09-26 23:08:44 +0200

    szerkesztő funkció beépítése

--- src/app/listdemo/listdemo.component.ts
+++ src/app/listdemo/listdemo.component.ts
@@ -9,6 +9,7 @@ import {EventModel} from './event-model';
 })
 export class ListdemoComponent {
   events: EventModel[];
+  modifyEvent: EventModel;
   constructor() {
     this.events = [
       {
@@ -28,17 +29,43 @@ export class ListdemoComponent {
       }
     ];
     console.log('megjott a listdemo cmp');
-    const puf = this.events.reduce((x: EventModel, y: EventModel) => {
-      // (logikai vuzsgálat) ? haigaz :hanem
-      return x.id > y.id ? x : y;
-    }).id;
-    console.log(puf);
+    // const puf = this.events.reduce((x: EventModel, y: EventModel) => {
+    //   // (logikai vuzsgálat) ? haigaz :hanem
+    //   return x.id > y.id ? x : y;
+    // }).id;
+    // console.log(puf);
+    this.modifyEvent = new EventModel('');
   }
-  add (newEventNameInput: HTMLInputElement, newEventPicInput: HTMLInputElement){
+  save (newEventNameInput: HTMLInputElement, newEventPicInput: HTMLInputElement){
+    if (this.modifyEvent.id === 0) {
+      // Új elemet hozunk létre...
     const maxId = this.events.reduce((x, y) => x.id > y.id ? x : y).id;
-    this.events = [...this.events, new EventModel(maxId + 1, newEventNameInput.value, newEventPicInput.value)];
+    this.events = [...this.events, new EventModel(newEventNameInput.value, maxId + 1, newEventPicInput.value)];
+    } else {
+      // itt tudjuk, hogy edit szakasz van, azaz meg kell keresni a meglfelelő elemet az id alapján
+      this.events = this.events.map((ev) => {
+        if (ev.id === this.modifyEvent.id) {
+          // itt tudjuk, hogy ezt az elemet kell szerkeszteni
+          return {
+            id: ev.id,
+            name: newEventNameInput.value,
+            pic: newEventPicInput.value
+          };
+        } else {
+          // itt tudjuk hogy nem akarunk módosítani
+          return ev;
+        }
+      });
+      // itt takarítsunk fel magunk után
+      this.modifyEvent = new EventModel('');
+    }
     newEventNameInput.value = '';
     newEventPicInput.value = '';
+  }
+  edit(id: number){
+    // ha biztos, hogy van ilyen
+    // Ha tudom hogy mindig csak 1 ilyen van
+    this.modifyEvent = this.events.filter( (ev) => ev.id === id) [0];
   }
   delete(id: number) {
     this.events = this.events.filter((ev: EventModel) => ev.id !== id);
